e22e85a2b8139ad8c7a1bce78e221463e053d213
Moksh-Mishraglbitm <25114cn353@glbitm.ac.in>
2026-06-03 00:25:50 +0530

    Add files via upload

---
+++ day5.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+int main(){
+//Q17.perfect number
+    int n,i,sum=0;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=1;i<=n-1;i++){
+        if(n%i==0)
+        sum=sum+i;
+    }
+    if(sum==n){
+        printf("its a perfect number");
+    }
+    else{
+        printf("not a perfect number");
+
+    }
+//Q20.to check strong number
+int org,v,p,fact=1,temp=0,j;
+printf("enter the number");
+scanf("%d",&p);
+org=p;
+while(p>0){
+    v=p%10;
+    fact=1;
+    for(j=1;j<=v;j++){
+        
+        fact=fact*j;
+    }
+    temp=temp+fact;
+    p=p/10;
+}
+if(temp==org){
+    printf("its a strong number");
+}
+else{
+    printf("not a strong number");
+
+}
+//Q19.factors of a number
+int largest=0,f,k,m=0,x,l;
+printf("enter the number");
+scanf("%d",&f);
+for(x=1;x<=f;x++){
+    if(f%x==0){
+        printf("%d\n",x);
+    
+        m=0;
+        for(k=1;k<=x;k++){
+            
+            if(x%k==0){
+            m=m+1;
+           }
+            }
+            if(m==2){
+                largest=x;
+            }
+
+        }
+
+}
+printf("\n%dlargest prime factor",largest);
+    
+  
+
+
+
+
+  
+
+//q20.largest prime factor
+
+
+
+
+
+
+
+
+
+
+
+
+}
